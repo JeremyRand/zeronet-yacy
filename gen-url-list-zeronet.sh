c6db9e83f59be7dc26cc0a85cb820984e42e5248
JeremyRand <biolizard89@gmail.com>
2018-07-28 19:53:52 +0000

    Use installed version of ncdumpzone.

--- gen-url-list-zeronet.sh
+++ gen-url-list-zeronet.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
-./ncdumpzone --ncdumpzone.format=url-list | sort --unique | tee url-list-zeronet-only.txt
+ncdumpzone --ncdumpzone.format=url-list | sort --unique | tee url-list-zeronet-only.txt
 cat url-list-zeronet-only.txt | sort --random-sort | tee url-list-random-zeronet-only.txt
