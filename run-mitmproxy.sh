f299af741c214820ceb2c169ab25559eb687418e
JeremyRand <jeremyrand@airmail.cc>
2019-10-22 05:10:59 +0000

    Rename Firefox ESR52 mitm-script.py

--- run-mitmproxy.sh
+++ run-mitmproxy.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-sudo mitmdump --listen-port=80 --mode reverse:http://127.0.0.1:81/ --set keep_host_header=true --scripts ./mitm-script.py
+sudo mitmdump --listen-port=80 --mode reverse:http://127.0.0.1:81/ --set keep_host_header=true --scripts ./mitm-script-firefox-esr52.py
